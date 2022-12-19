cb13ad53e808749556724d2e405ef5d3e7825d6a
robert lee <shat0806449@gmail.com>
2022-12-19 16:05:55 +0800

    Initial Commit

---
+++ psychologicalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  psychologicalTest
+//
+//  Created by user on 19/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
